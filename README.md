16ae892a050109b2469121745c1e989fa5aef080
lza_menace <lza_menace@protonmail.com>
2022-03-26 22:29:42 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nekocore-claim-checker
+
 Helper utility to check if a token has been claimed
+
+## dev
+
+`python3 -m http.server`
+
+## prod
+
+host it somewhere, dev will know what to do with it
---
+++ static/js/main.js
@@ -0,0 +1,86 @@
+window.addEventListener('DOMContentLoaded', async () => {
+  let accounts;
+  const onboarding = new MetaMaskOnboarding();
+  const checkTokenClaimedButton = document.getElementById('checkTokenClaimed');
+  const tokenIdInput = document.getElementById('tokenId');
+
+  // Offer to install MetaMask if it's not installed nor do we
+  // detect a replacement such as Coinbase wallet
+  if (!MetaMaskOnboarding.isMetaMaskInstalled() && !window.ethereum) {
+    alert('This site requires a browser wallet addon, such as Coinbase wallet or MetaMask. Redirecting you to a page to download MetaMask.');
+    onboarding.startOnboarding();
+  } else if (accounts && accounts.length > 0) {
+    onboarding.stopOnboarding();
+  }
+  await switchNetwork();
+  await getMMAccount();
+
+  checkTokenClaimedButton.onclick = async () => {
+    await _checkTokenClaimed();
+  };
+
+  tokenIdInput.onkeyup = async (e) => {
+    if (e.key == 'Enter') {
+      await _checkTokenClaimed();
+    }
+  }
+
+  ethereum.on('accountsChanged', function (accounts) {
+    window.location.href = '';
+  })
+});
+
+async function _checkTokenClaimed() {
+  try {
+    await checkTokenClaimed();
+  } catch(e) {
+    console.log(e)
+    return false;
+  }
+}
+
+async function getMMAccount() {
+  try {
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    });
+    const account = accounts[0];
+    return account;
+  } catch(e) {
+    updateMintMessage(`Something went wrong. Refresh and try again.`);
+  }
+}
+
+async function switchNetwork(){
+  // don't do this if no metamask (errors on coinbase wallet)
+  if (!MetaMaskOnboarding.isMetaMaskInstalled()) {
+    return false;
+  }
+  await ethereum.request({
+    method: 'wallet_switchEthereumChain',
+    params: [{ chainId: '0x1' }],
+  });
+}
+
+async function checkTokenClaimed() {
+  const w3 = new Web3(Web3.givenProvider || "http://127.0.0.1:7545");
+  const walletAddress = await getMMAccount();
+  let res = document.getElementById('claimedResult');
+  let tokenId = document.getElementById('tokenId').value;
+  if (tokenId <= 0 || tokenId > 9999 || isNaN(tokenId)) {
+    alert('Invalid token ID supplied. Try again (1-9999)');
+  }
+
+  const contract = new w3.eth.Contract(abi, address, {from: walletAddress});
+  const claimed = await contract.methods.TIMES_CLAIMED(tokenId).call();
+
+  if (claimed == 0) {
+    res.innerHTML = `Token ${tokenId} not yet claimed!`;
+    res.classList.add('fail');
+    res.classList.remove('success');
+  } else {
+    res.innerHTML = `Token ${tokenId} already claimed!`;
+    res.classList.add('success');
+    res.classList.remove('fail');
+  }
+}
